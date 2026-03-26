e2fb948db734c94bb501c300d5894b0167773dca
anasakhssas <anasakhssas357@gmail.com>
2026-03-26 14:33:16 +0100

    Add a predefined list for Major field and force the recruiter to choose from the list for Major and Expected Degree

--- __manifest__.py
+++ __manifest__.py
@@ -6,6 +6,7 @@
     'depends': ['hr_recruitment'],  # Crucial: This ensures hr.job exists
     'data': [
         'security/ir.model.access.csv', # You need to give yourself permission to run the code
+        'data/hr_job_major_data.xml',
         'views/hr_applicant_views.xml',
         'views/hr_job_views.xml',
     ],
--- models/hr_job.py
+++ models/hr_job.py
@@ -1,12 +1,29 @@
 from odoo import fields, models
 
 
+class HrJobMajor(models.Model):
+    _name = 'hr.job.major'
+    _description = 'Job Major'
+    _order = 'name'
+
+    name = fields.Char(required=True)
+
+
 class HrJob(models.Model):
     _inherit = 'hr.job'
 
-    major = fields.Char(
+    major_id = fields.Many2one(
+        comodel_name='hr.job.major',
         string='Major',
+        ondelete='restrict',
         help='Primary field of study expected for this job (e.g., Computer Science, Finance).',
+    )
+
+    major = fields.Char(
+        related='major_id.name',
+        string='Major',
+        store=True,
+        readonly=True,
     )
 
     min_exp_years = fields.Float(
